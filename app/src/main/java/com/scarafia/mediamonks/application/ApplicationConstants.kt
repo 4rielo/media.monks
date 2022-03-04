f658878a6c1837e329519ac312ec60557562df83
Ariel <ascarafia@gmail.com>
2022-03-03 21:45:18 -0300

    Added Splash Screen With animation lootie and safety timeout.

--- app/src/main/java/com/scarafia/mediamonks/application/ApplicationConstants.kt
+++ app/src/main/java/com/scarafia/mediamonks/application/ApplicationConstants.kt
@@ -3,4 +3,6 @@ package com.scarafia.mediamonks.application
 object ApplicationConstants {
     internal const val BASE_URL: String = "https://jsonplaceholder.typicode.com/"
     internal const val API_TIME_OUT = 20L
+    internal const val SPLASH_SAFE_TIMEOUT_DURATION = 7000L
+    internal const val SPLASH_SAFE_TIMEOUT_STEP = 1000L
 }
---
+++ app/src/main/java/com/scarafia/mediamonks/presentation/screens/splash/SplashActivity.kt
@@ -0,0 +1,65 @@
+package com.scarafia.mediamonks.presentation.screens.splash
+
+import android.R
+import android.animation.Animator
+import android.content.Intent
+import android.os.Bundle
+import android.os.CountDownTimer
+import androidx.appcompat.app.AppCompatActivity
+import com.scarafia.mediamonks.databinding.ActivitySplashBinding
+import com.scarafia.mediamonks.presentation.screens.homescreen.HomeActivity
+import org.koin.androidx.viewmodel.ext.android.viewModel
+
+
+class SplashActivity: AppCompatActivity() {
+
+    private lateinit var binding: ActivitySplashBinding
+
+    private val splashViewModel: SplashViewModel by viewModel()
+    private lateinit var homeIntent: Intent
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivitySplashBinding.inflate(layoutInflater)
+
+        val animationView = binding.laLootieAnimation
+        homeIntent = Intent(this, HomeActivity::class.java)
+
+        animationView
+            .addAnimatorListener(object : Animator.AnimatorListener{
+            override fun onAnimationRepeat(animation: Animator?) {
+            }
+
+            override fun onAnimationEnd(animation: Animator?) {
+                startActivity(intent)
+            }
+
+            override fun onAnimationCancel(animation: Animator?) {
+            }
+
+            override fun onAnimationStart(animation: Animator?) {
+            }
+
+        })
+
+        setContentView(binding.root)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        setObservers()
+    }
+
+    private fun setObservers() {
+        observeSafetyTimeout()
+    }
+
+    private fun observeSafetyTimeout(){
+        splashViewModel.safeTimeout.observe(this){
+            if(it) {
+                startActivity(homeIntent)
+            }
+        }
+    }
+
+}
